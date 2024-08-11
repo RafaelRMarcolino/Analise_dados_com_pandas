f08cd8ae6da352ed4148c295122d7e48be960c8b
Rafael <ralfrr55@gmail.com>
2024-08-11 19:16:56 -0300

    ....

--- 01/01.py
+++ 01/01.py
@@ -1,9 +1,4 @@
 # %%
-print('Anlise de dados Pandas')
-
-# %%
-
-from statistics import median
 import pandas as pd
 
 # %%
@@ -12,53 +7,87 @@ idades = [30, 42, 90, 34]
 idades
 
 # %%
-
 media = sum(idades) / len(idades)
-
-# %%
-#desvio padrão
 
 total = 0
 for i in idades:
-    total += (media -1) **1
+    total += (media - i)**2
 
-variancia = total / (len(idades) - 1)
-
-print(variancia)
-
+variancia = total / (len(idades) - 1) 
+variancia
 
 # %%
-
+# Transformação para séries Pandas
 series_idades = pd.Series(idades)
 series_idades
 
 # %%
+# Métodos da séries pandas
+# Média
 series_idades.mean()
 
 # %%
+# Variância
 series_idades.var()
+
+# Desvio padrão
+series_idades.std()
+
+# %%
+# Mediana
+series_idades.median()
+
+# %%
+# 3o Quartil
+series_idades.quantile(0.75)
+
+# %%
+# Sumarização
+series_idades.describe()
+
+# %%
+# Dimensão da série
+series_idades.shape
+
+# %%
+# Navegando na lisa
+idades[0]
+
+# %%
+# Navegando na série
+series_idades[3]
+
+# %%
+# Alterando index da série
+series_idades.index = ['r', 'a', 'f', 'a']
+
+# %%
+# Navegando nos índices novos
+series_idades['t']
 
 # %%
 
-series_idades.median()
+series_idades.index = [40, 10, 30, 20]
+series_idades
 
-series_idades.std()
+# %%
+series_idades
 
+# %%
+series_idades.iloc[2:4]
 
-series_idades.describe
-
-
-series_idades.shape
-
-#trocar indices
-series_idades.index = ['r', 'a', 'f', 'e']
-
-series_idades[2]
-series_idades.describe
-
-#iloc pega pela posição permite que voce pague os dados mesmo se o indidece estiver embatalhjado o iloc garante o que vc esta pegando esta certo
+# %%
 series_idades.iloc[0:2]
 
-# Pega pelo indice
-# series_idades.loc[0]
+# %%
+series_idades.loc[40]
 
+# %%
+series_idades.name = 'idades'
+series_idades
+
+# %%
+
+series_idades = pd.Series(idades, name="idades")
+series_idades
+# %%
