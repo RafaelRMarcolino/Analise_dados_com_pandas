b591fcadcad39e44da579eb0baf2563af8da8b12
Rafael Reinaldo Marcolino <ralfrr55@gmail.com>
2024-10-09 22:30:32 -0300

    faker

--- estatistica/data.py
+++ estatistica/data.py
@@ -1,24 +1,51 @@
-from faker import Faker
-import random
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import desc
+import pandas as pd
+import numpy as np
 
-spark = SparkSession.builder.appName('').getOrCreate()
+sexo = {0: 'Masculino', 1: 'Feminino'}
+cor = {0: 'Indigena', 1: 'Branca', 3: 'Preta', 4: 'Amarela', 5: 'Parda', 6: 'Sem Declaração'}
 
-sexo = {0: 'Masculino',
-        1: 'Feminino'}
+ufs = [
+    'AC',  # Acre
+    'AL',  # Alagoas
+    'AP',  # Amapá
+    'AM',  # Amazonas
+    'BA',  # Bahia
+    'CE',  # Ceará
+    'DF',  # Distrito Federal
+    'ES',  # Espírito Santo
+    'GO',  # Goiás
+    'MA',  # Maranhão
+    'MT',  # Mato Grosso
+    'MS',  # Mato Grosso do Sul
+    'MG',  # Minas Gerais
+    'PA',  # Pará
+    'PB',  # Paraíba
+    'PR',  # Paraná
+    'PE',  # Pernambuco
+    'PI',  # Piauí
+    'RJ',  # Rio de Janeiro
+    'RN',  # Rio Grande do Norte
+    'RS',  # Rio Grande do Sul
+    'RO',  # Rondônia
+    'RR',  # Roraima
+    'SC',  # Santa Catarina
+    'SP',  # São Paulo
+    'SE',  # Sergipe
+    'TO'   # Tocantins
+]
 
-cor = {0: 'Indigena',
-       1: 'Branca',
-       3: 'Preta',
-       4: 'Amarela',
-       5: 'Parda',
-       6: 'Sem Declaração'}
+num = 1000
 
+data = {
+    'UF': np.random.choice(ufs, num),
+    'Sexo': np.random.choice(list(sexo.values()), num),
+    'Idade': np.random.randint(18, 70, num),
+    'Cor': np.random.choice(list(cor.values()), num),
+    'Anos de estudo': np.random.randint(0, 21, num),  # De 0 a 20
+    'Renda': np.random.randint(1000, 20000, num),
+    'Altura': np.random.uniform(1.5, 2.0, num).round(2)
+}
 
+df = pd.DataFrame(data)
 
-def fake_renda(num):
-    data = []
-    for _ in range(num):
-        renda = f"nm{str(random.randint(1, 300)).zfill(7)}"
-
+print(df.head(100))
